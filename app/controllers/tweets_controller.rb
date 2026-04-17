466269196a06893eded4a8003c98f7be51559c7f
Cauanz <61758981+Cauanz@users.noreply.github.com>
2026-04-17 11:33:41 -0300

    🚧 Implementar funcionalidades básicas para gerenciamento de tweets

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,38 @@
 class TweetsController < ApplicationController
 
+  # /tweets - GET TWEETS
   def index
     @tweets = Tweet.all
+  end
+
+  # /tweets - GET TWEETS FORM
+  def new
+    @tweet = Tweet.new
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+  
+  
+  # /tweets - POST TWEETS FORMDATA
+  # TODO - NÃO FUNCIONANDO DE NOVO
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    if @tweet.save
+      flash[:success] = "Tweet successfully created"
+      redirect_to @tweet
+      # redirect_to tweets_path
+    else
+      flash[:error] = "Something went wrong"
+      render :new
+    end
+  end
+  
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 
 end
