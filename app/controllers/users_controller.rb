2b71e6dff15abadbd9cb68dff8cb0430bfff030c
Cauanz <61758981+Cauanz@users.noreply.github.com>
2026-04-27 11:36:00 -0300

    feat: add implement edit profile functions and user validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_user, only: %i[show]
+  before_action :set_user, only: %i[show edit update]
 
 
   def show
@@ -8,14 +8,29 @@ class UsersController < ApplicationController
   end
 
 
+  def edit
+    
+  end
+
+  def update
+    if @user.update(validate_user)
+      redirect_to user_path(current_user)
+    else
+      render user_path(current_user)
+    end
+  end
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :bio, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :nickname, :bio, :avatar])
   end
-
   
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def validate_user
+    params.require(:user).permit(:username, :nickname, :avatar, :bio)
   end
 end
